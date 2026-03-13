b9f5e8f36b55c056ad2e2903515b17651c8e7893
minthwayhtut <minthwayhtut568@gmail.com>
2026-03-13 22:54:08 +0100

    Modifying main.py file

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,3 @@ async def remove_background(file: UploadFile = File(...)):
     img_byte_arr.seek(0)
 
     return StreamingResponse(img_byte_arr, media_type="image/png")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
