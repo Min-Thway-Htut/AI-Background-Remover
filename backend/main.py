4cd9e478cb32a2d2688fe8ac3deac492d94e671a
minthwayhtut <minthwayhtut568@gmail.com>
2026-03-13 21:29:10 +0100

    Initial commit - clean repository without venv

---
+++ backend/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from rembg import remove
+from PIL import Image
+import io
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/remove-background")
+async def remove_background(file: UploadFile = File(...)):
+
+    input_image = Image.open(file.file)
+
+    output = remove(input_image)
+
+    img_byte_arr = io.BytesIO()
+    output.save(img_byte_arr, format="PNG")
+    img_byte_arr.seek(0)
+
+    return StreamingResponse(img_byte_arr, media_type="image/png")
