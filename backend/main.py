cea9bbe8379c389395b7cadea5e39f01d2a144d8
minthwayhtut <minthwayhtut568@gmail.com>
2026-03-13 22:17:08 +0100

    Edit main.py for production

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from rembg import remove
 from PIL import Image
 import io
+import os
+import uvicorn 
 
 app = FastAPI()
 
@@ -27,3 +29,7 @@ async def remove_background(file: UploadFile = File(...)):
     img_byte_arr.seek(0)
 
     return StreamingResponse(img_byte_arr, media_type="image/png")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
